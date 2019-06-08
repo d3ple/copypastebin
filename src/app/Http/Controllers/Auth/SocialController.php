7bdc059fe4bc8fd7acad2885d3c9b163db072235
Danil <d.danil.d2@gmail.com>
2019-06-09 03:40:36 +0700

    Fix case when social auth user has same email as normal auth user

--- src/app/Http/Controllers/Auth/SocialController.php
+++ src/app/Http/Controllers/Auth/SocialController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Exception;
 use Redirect;
 use Socialite;
+use Illuminate\Support\MessageBag;
 
 
 class SocialController extends Controller
@@ -25,9 +27,17 @@ class SocialController extends Controller
             return Redirect::to('auth/github');
         }
 
-        $authUser = User::firstOrCreate(
-            ['auth_provider_user_id' => $user->id, 'auth_provider' => $provider],
-            ['name' => $user->name, 'email' => $user->email]);
+
+        try {
+            $authUser = User::firstOrCreate(
+                ['auth_provider_user_id' => $user->id, 'auth_provider' => $provider],
+                ['name' => $user->name, 'email' => $user->email]);
+        } catch (Exception $e) {
+            $errors = new MessageBag;
+            $errors->add('email', 'The same email is already registered. Use it to log in.');
+            return view('auth.login')->withErrors($errors);
+        }
+
         auth()->login($authUser);
         return redirect()->to('/');
     }
